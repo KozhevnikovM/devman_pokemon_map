45bec695e23c84caf562bd6c5813ac6e548c295e
Administrator <admin@example.com>
2021-03-16 18:49:04 +0300

    pockemons entiti loadming from database

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # your models here
-class Pockemon(models.Model):
+class Pokemon(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(null=True, blank=True)
 
@@ -11,7 +11,7 @@ class Pockemon(models.Model):
 class PokemonEntity(models.Model):
     lat = models.FloatField()
     lon = models.FloatField()
-    pockemon = models.ForeignKey(Pockemon, on_delete=models.SET_NULL, null=True)
+    pockemon = models.ForeignKey(Pokemon, on_delete=models.SET_NULL, null=True)
     appeared_at = models.DateTimeField(blank=True, null=True)
     disappeared_at = models.DateTimeField(blank=True, null=True)
     level = models.IntegerField(null=True, blank=True)
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -4,7 +4,7 @@ import json
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
-from .models import Pockemon
+from .models import Pokemon, PokemonEntity
 
 
 MOSCOW_CENTER = [55.751244, 37.618423]
@@ -24,24 +24,25 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    with open("pokemon_entities/pokemons.json", encoding="utf-8") as database:
-        pokemons = json.load(database)['pokemons']
-
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon in pokemons:
-        for pokemon_entity in pokemon['entities']:
-            add_pokemon(
-                folium_map, pokemon_entity['lat'], pokemon_entity['lon'], pokemon['img_url'])
+
+    for pokemon_entity in PokemonEntity.objects.all():
+        add_pokemon(
+            folium_map, pokemon_entity.lat, pokemon_entity.lon, request.build_absolute_uri(
+                pokemon_entity.pockemon.image.url
+            ))
 
     pokemons_on_page = []
-    for pokemon in Pockemon.objects.all():
+
+    for pokemon in Pokemon.objects.all():
         image = pokemon.image.url if pokemon.image else None
         pokemons_on_page.append({
             'pokemon_id': pokemon.id,
             'img_url': image,
             'title_ru': pokemon.title,
         })
-    pockemons = Pockemon.objects.all()
+    
+    pockemons = Pokemon.objects.all()
 
     return render(request, "mainpage.html", context={
         'map': folium_map._repr_html_(),
